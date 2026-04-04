ccf340850c184299275ff8045a7169f851804055
urvashidab <urvashi.dabgotra11@gmail.com>
2026-04-05 00:50:21 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,19 @@ dotenv.config();
 import express from "express";
 import mongoose from "mongoose";
 import connectDB from "./config/db.js";
+import bookRoutes from "./routes/bookRoutes.js";
 
 const app = express();
 
+//connect database
 connectDB();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//routes
+app.use("api/books", bookRoutes);
 
 // landing page
 app.get("/", (req, res) => {
